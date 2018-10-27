52bd89efc6fd2df6b9b79151638fed2949261053
JamesWrightDev <32226552+JamesWrightDev@users.noreply.github.com>
2018-10-27 17:40:41 +0100

    Add filter cheap, closest and best pints

--- config/google.php
+++ config/google.php
@@ -2,6 +2,7 @@
 
 return [
     'places' => [
-        'key' => env('GOOGLE_PLACES_API_KEY', null),
+        'key' => env('GOOGLE_PLACES_API_KEY', AIzaSyDlH9Nz1q8nhohl9pOdVHMowyY6R3RDZ18),
     ],
 ];
+// Google API KEY AIzaSyDlH9Nz1q8nhohl9pOdVHMowyY6R3RDZ18
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/results', function(){
+    $response = GooglePlaces::nearbySearch('51.528820, -0.048160', '800', ['type'=>'bar', 'minprice'=>'1', 'rankby' => 'distance']);
+    $results = $response['results'];
+    $query = array();
+    $count = 0;
+    foreach($results as $result){
+        $a = array('name' => $result['name'],'rating' => $result['rating'], 'price_level' => $result['price_level']);
+        array_push($query, $a);
+        $result['name'];
+        $result['rating'];
+        $result['price_level'];
+    }
+    $maxBestPint = 0;
+    $minCheapPint = 6;
+    $bestPint;
+    $cheapPint;
+    $closePint = $query[0];
+
+    foreach($query as $k=>$v)
+    {
+        if($v['price_level']<$minCheapPint)
+        {
+            $minCheapPint = $v['price_level'];
+            $cheapPint = $v;
+        }
+        
+        if($v['rating']>$maxBestPint)
+        {
+            $max = $v['rating'];
+            $bestPint = $v;
+        }
+    }
+
+
+    $returnThis = array_search(max($query),$query);
+
+    $closePint = $query[0];
+
+    dd($cheapPint);
+
+  
+});
