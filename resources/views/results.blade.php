b25ee78fbb32a910f99efabdf80499a2eb33a35a
JamesWrightDev <32226552+JamesWrightDev@users.noreply.github.com>
2018-10-30 08:35:05 +0000

    Fix front end

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -5,7 +5,7 @@
           <div class="is-flex is-horizontal-center flex-column">
             <h2 class="is-size-4 has-text-centered">Closest Pint Near You</h2>
             <h3 class="is-size-2 has-text-centered">{{ $closePint['name'] }}</h3>
-            <a class='button is-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $bestPint['place_id'] }}">Directions</a>
+            <a class='button button-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $bestPint['place_id'] }}">Directions</a>
           </div>
         </div>
       </div>
@@ -14,7 +14,7 @@
           <div class="is-flex is-horizontal-center flex-column">
             <h2 class="is-size-4 has-text-centered">Best Pint Near You</h2>
             <h3 class="is-size-2 has-text-centered">{{ $bestPint['name'] }}</h3>
-            <a class='button is-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $bestPint['place_id'] }}">Directions</a>
+            <a class='button button-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $bestPint['place_id'] }}">Directions</a>
           </div>
         </div>
       </div>
@@ -23,7 +23,7 @@
           <div class="is-flex is-horizontal-center flex-column">
             <h2 class="is-size-4 has-text-centered">Cheapest Pint Near You</h2>
             <h3 class="is-size-2 has-text-centered">{{ $cheapPint['name'] }}</h3>
-            <a class='button is-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $cheapPint['place_id'] }}">Directions</a>
+            <a class='button button-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $cheapPint['place_id'] }}">Directions</a>
           </div>
         </div>
       </div>
