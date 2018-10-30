2bd6d5b18d09c3523a842068d2741a11ea901e0e
JamesWrightDev <32226552+JamesWrightDev@users.noreply.github.com>
2018-10-30 19:37:25 +0000

    'test'

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -4,7 +4,7 @@
   <div class="columns">
         <div class="column is-three-fifths is-offset-one-fifth ">
           <div class="is-flex is-horizontal-center flex-column">
-            <h2 class="is-size-4 has-text-centered">Closest Pint Near You</h2>
+            <h2 class="is-size-4 has-text-centered">Closest Pint Near You:</h2>
             <h3 class="is-size-2 has-text-centered">{{ $closePint['name'] }}</h3>
             <a class='button button-primary result-button' href="https://www.google.com/maps/search/?api=1&query={{ $closePint['name'] }}&query_place_id={{ $closePint['place_id'] }}">Directions</a>
           </div>
@@ -13,7 +13,7 @@
       <div class="columns">
         <div class="column is-three-fifths is-offset-one-fifth ">
           <div class="is-flex is-horizontal-center flex-column">
-            <h2 class="is-size-4 has-text-centered">Best Pint Near You</h2>
+            <h2 class="is-size-4 has-text-centered">Best Pint Near You:</h2>
             <h3 class="is-size-2 has-text-centered">{{ $bestPint['name'] }}</h3>
             <a class='button button-primary result-button' href="https://www.google.com/maps/search/?api=1&query={{ $bestPint['name'] }}&query_place_id={{ $bestPint['place_id'] }}">Directions</a>
           </div>
@@ -22,7 +22,7 @@
       <div class="columns">
         <div class="column is-three-fifths is-offset-one-fifth ">
           <div class="is-flex is-horizontal-center flex-column">
-            <h2 class="is-size-4 has-text-centered">Cheapest Pint Near You</h2>
+            <h2 class="is-size-4 has-text-centered">Cheapest Pint Near You:</h2>
             <h3 class="is-size-2 has-text-centered">{{ $cheapPint['name'] }}</h3>
             <a class='button button-primary result-button' href="https://www.google.com/maps/search/?api=1&query={{ $cheapPint['name'] }}&query_place_id={{ $cheapPint['place_id'] }}">Direction</a>
           </div>
