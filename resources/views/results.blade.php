5cdbd8de69b950a37d79047bb5bf4bb79f529c7f
James <jameswrightfilm@gmail.com>
2018-10-30 13:06:59 +0000

    google map links fix

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -6,7 +6,7 @@
           <div class="is-flex is-horizontal-center flex-column">
             <h2 class="is-size-4 has-text-centered">Closest Pint Near You</h2>
             <h3 class="is-size-2 has-text-centered">{{ $closePint['name'] }}</h3>
-            <a class='button button-primary' href="https://www.google.com/maps/place/?q=place_id:{{ $bestPint['place_id'] }}">Directions</a>
+            <a class='button button-primary' href="https://www.google.com/maps/search/?api=1&query={{ $closePint['name'] }}&query_place_id={{ $closePint['place_id'] }}">Directions</a>
           </div>
         </div>
       </div>
@@ -15,7 +15,7 @@
           <div class="is-flex is-horizontal-center flex-column">
             <h2 class="is-size-4 has-text-centered">Best Pint Near You</h2>
             <h3 class="is-size-2 has-text-centered">{{ $bestPint['name'] }}</h3>
-            <a class='button button-primary' href="https://maps.google.com/maps/place/?q=place_id:{{ $bestPint['place_id'] }}">Directions</a>
+            <a class='button button-primary' href="https://www.google.com/maps/search/?api=1&query={{ $bestPint['name'] }}&query_place_id={{ $bestPint['place_id'] }}">Directions</a>
           </div>
         </div>
       </div>
@@ -25,7 +25,6 @@
             <h2 class="is-size-4 has-text-centered">Cheapest Pint Near You</h2>
             <h3 class="is-size-2 has-text-centered">{{ $cheapPint['name'] }}</h3>
             <a class='button button-primary' href="https://www.google.com/maps/search/?api=1&query={{ $cheapPint['name'] }}&query_place_id={{ $cheapPint['place_id'] }}">Test</a>
-            <a class='button button-primary' href="https://maps.google.com/?q=place_id:{{ $cheapPint['place_id'] }}">Directions</a>
           </div>
         </div>
       </div>
